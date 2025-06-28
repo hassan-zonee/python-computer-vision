52cfaa43188ef6e9866be2e9f906bdd5901f3e85
hassan <hassan.zoneee@gmail.com>
2025-06-28 10:22:59 +0100

    video processing

--- learning-opencv/video-processing-simple.py
+++ learning-opencv/video-processing-simple.py
@@ -1,29 +1,60 @@
-import cv2
-
-
-cap = cv2.VideoCapture('../testvid.mp4')
-fps = cap.get(cv2.CAP_PROP_FPS)
-delay = int(1000/fps)
-
-h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * 0.5)
-w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * 0.5)
-
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output = cv2.VideoWriter('1.mp4', fourcc, fps, (w, h))
-
-while True:
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-
-    resized = cv2.resize(frame, (w, h), interpolation=cv2.INTER_AREA) 
-    output.write(resized)
-    cv2.imshow('Video', resized)
-
-    if(cv2.waitKey(delay) & 0xff==ord('q')):
-        break
-
-cap.release()
-output.release()
-cv2.destroyAllWindows()
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "42368b5d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import cv2"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "ac93438c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cap = cv2.VideoCapture('testvid.mp4')\n",
+    "\n",
+    "while True:\n",
+    "    ret, frame = cap.read()\n",
+    "\n",
+    "    if not ret:\n",
+    "        break\n",
+    "\n",
+    "    cv2.imshow('Video', frame)\n",
+    "\n",
+    "    if(cv2.waitKey(1) and 0xff == ord('q')):\n",
+    "        break\n",
+    "\n",
+    "cap.release()\n",
+    "cv2.destroyAllWindows()\n",
+    "    "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "env",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
