81d69ee93baee433ed21b9dca614ee8327657b65
Hassan <hassan.zoneee@gmail.com>
2025-07-02 10:23:21 +0100

    changes

--- learning-yolo/yolo-getting-started.py
+++ learning-yolo/yolo-getting-started.py
@@ -1,6 +1,24 @@
 from ultralytics import YOLO
+import cv2
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n-cls.pt")
 
-predict = model.predict(source="https://ultralytics.com/images/bus.jpg", show=True)
-predict.show()
+cap = cv2.VideoCapture(0)
+fps = cap.get(cv2.CAP_PROP_FPS)
+delay = int(1000 / fps)
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    result = model(frame)
+
+    cv2.imshow("YOLOv8 Detection", result[0].plot())
+
+    if cv2.waitKey(delay) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
